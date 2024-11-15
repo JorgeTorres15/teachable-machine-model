77c736fd367eefed1d64bf56dcd01f4745545008
Mr.ExtremeProgramming <162726095+JorgeTorres15@users.noreply.github.com>
2024-11-14 23:58:51 -0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,24 @@
-// service-worker.js
-
 self.addEventListener('install', (event) => {
-    console.log('Service Worker instalado');
-});
-
-self.addEventListener('activate', (event) => {
-    console.log('Service Worker activado');
+  event.waitUntil(
+      caches.open('my-cache').then((cache) => {
+          return cache.addAll([
+              '/', // La raíz de la PWA
+              '/index.html', // Archivo HTML
+              '/app.js', // Script de la aplicación
+              'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs', // Cargar TensorFlow.js desde CDN
+              'https://jorgetorres15.github.io/teachable-machine-model/model.json', // El modelo de Teachable Machine
+              'https://jorgetorres15.github.io/teachable-machine-model/group1-shard1of1.bin', // Binarios del modelo
+              '/icons/icon-192x192.png', // Icono de la PWA
+              '/icons/icon-512x512.png'  // Icono de la PWA
+          ]);
+      })
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+      caches.match(event.request).then((response) => {
+          return response || fetch(event.request);
+      })
+  );
 });
